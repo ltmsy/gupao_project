439fa7bf6e80b0895352c544e3ae25b0edba42bc
李通明 <424031810@qq.com>
2018-05-22 11:21:53 +0800

    代理模式

---
+++ com.gupao.proxy/src/gupao/proxy/UserService.java
@@ -0,0 +1,13 @@
+package gupao.proxy;
+
+/**
+ * @author
+ * @create 2018-05-22 10:17
+ **/
+public interface UserService {
+
+    /**
+     * 目标方法
+     */
+    public abstract void add();
+}
---
+++ com.gupao.proxy/src/gupao/proxy/UserServiceImpl.java
@@ -0,0 +1,12 @@
+package gupao.proxy;
+
+/**
+ * @author
+ * @create 2018-05-22 10:33
+ **/
+public class UserServiceImpl implements UserService {
+    @Override
+    public void add() {
+        System.out.println("--------------------add---------------");
+    }
+}
